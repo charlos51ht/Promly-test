9f74d29e505c08f0779c04151da4ff62e1c3e90b
psher0226 <52217039+psher0226@users.noreply.github.com>
2022-03-23 09:23:52 -0400

    Delete Goal.kit Copy

--- Goal.kt
+++
@@ -1,5 +0,0 @@
-package com.example.promly.TwentyByTwenty
-
-import com.example.promly.R
-
-data class Goal(var goalTitle: String? = null, var goalImage: Int = R.drawable.empty_goal, var goalStatus: Int? = null)
